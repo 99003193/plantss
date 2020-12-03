6a185f1616cd0403408352570b5b937516119969
99003193 <bandiramesh772@gmail.com>
2020-12-03 23:09:58 +0530

    First commit

--- main.c
+++ main.c
@@ -5,33 +5,112 @@ int main()
 {
 	char a[10];
 	int plants=0;
-	printf("ENTER DISEASE NAME AMONG THESE \n Downy mildew \n Powdery mildew \n Anthracnose \n Alternaria blight \n ");
+	printf("ENTER DISEASE NAME AMONG THESE \n fever \n malaria \n covid \n diarrhea \n dengue \n tuberculosis \n arthritis \n mouth ulcer \n polio \n ");
 	printf("*******ENTER DISEASE NAME******* \n ");
 	scanf("%s",a);
 	plants=plant(a);
-	if(plants==1){
+	if(plants==1){	
 		printf("SYMPTOMS:: \n ");
-		printf("Symptoms of this disease are frequently confused with those of powdery mildew. Infected leaves develop pale yellow-green lesions which gradually turn brown. Severely infected leaves often drop prematurely. \n ");
-		printf("***TREATMENT SUGGESTED*** \n ");
-		printf("Penn State Extension recommends spraying grapevines with a fungicide just before blossoms open, seven to 10 days later, and 10 to 14 days after that, followed by a final application three weeks later \n ");
+		printf("sweating \n shivering \n headache \n muscle aches \n loss of appetite \n dehydration \n general weakness \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("paracetamol \n ketoprofen \n ibuprofen \n aspirin \n ");
+		printf("INHOME REMEDIES ARE: \n ");
+		printf("Take a bath in lukewarm water & place cold wash cloths on your forehead \n ");
 	}
 	if(plants==2){
 		printf("SYMPTOMS:: \n ");
-		printf("Diseased leaves appear whitish gray, dusty, or have a powdery white appearance. Petioles, cluster stems, and green shoots often look distorted or stunted.  \n ");
-		printf("***TREATMENT SUGGESTED*** \n ");
-		printf("Apply a 1-2% oil treatment to the vines within 2 days of the water application. In general, petroleum-based oils work better than plant-based oils. Thorough spray coverage is essential for good results. Forget using bicarbonate products; they do not work when the powdery mildew pressure is very high. \n ");
+		printf("Fever \n Chills \n Headache \n Nausea and vomiting \n Muscle pain and fatigue \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("Combination of atovaquone and proguanil (Malarone) \n Quinine sulfate (Qualaquin) with doxycycline (Vibramycin, Monodox, others) \n Mefloquine \n Primaquine phosphate \n ");
+		printf("INHOME REMEDIES ARE: \n ");
+		printf("Cinnamon \n Turmeric \n Orange juice \n Ginger \n Apple cider vinegar \n");
 	}
 	if(plants==3){
 		printf("SYMPTOMS:: \n ");
-		printf("The disease appears first as dark red spots on the berry. Later, these spots are circular, sunken, ashy-gray and in late stages these spots are surrounded by a dark margin which gives it the “bird’s-eye rot” appearance. The spots vary in size from 1/4 inch in diameter to about half the fruit.\n ");
-		printf("***TREATMENT SUGGESTED*** \n ");
-		printf("Remove and destroy any infected plants in your garden. For trees, prune out the dead wood and destroy the infected leaves. \n You can try spraying your plants with a copper-based fungicide, though be careful because copper can build up to toxic levels in the soil for earthworms and microbes.\n ");
+		printf("Fever or chills \n Cough \n Shortness of breath or difficulty breathing \n Fatigue \n Muscle or body aches \n Headache \n New loss of taste or smell \n Sore throat \n Congestion or runny nose \n Nausea or vomiting \n Diarrhea \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("Hydroxychloroquine \n Methylprednisolone and Dexamethasone \n ");
+		printf("INHOME REMEDIES ARE: \n");
+		printf("Maintain a daily routine, including showering and getting dressed \n Take breaks from COVID-19 news, including social media \n Eat healthy meals and stay hydrated \n ");
 	}
 	if(plants==4){
 		printf("SYMPTOMS:: \n ");
-		printf("Symptoms in the form of dark brown-purplish patches appear on the infected berries, rachis and bunch stalk with the shoots. \n ");
-		printf("***TREATMENT SUGGESTED*** \n ");
-		printf("Treatment for alternaria requires fungicide to be sprayed directly on infected plants, as well as improvements in sanitation and crop rotation to prevent future outbreaks.\n Organic gardeners are limited to sprays of captan or copper fungicides, making control much more challenging. \n ");
+		printf("Frequent loose, watery stools \n Abdominal cramps \n Abdominal pain \n Fever \n Bleeding \n Lightheadedness or dizziness from dehydration \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("Imodium (loperamide) and Pepto-Bismol or Kaopectate \n ");
+		printf("INHOME REMEDIES ARE: \n ");
+		printf("foods rich in pectin \n such as fruit foods high in potassium \n such as potatoes and sweet potatoes foods with electrolytes \n such as miso soup and sports drinks cooked \n soft vegetables adequate amounts of protein \n");
 	}
+	if(plants==5){
+		printf("SYMPTOMS:: \n ");
+		printf("Sudden high fever \n Severe headaches Pain behind the eyes \n Severe joint and muscle pain \n Fatigue NauseaVomiting \n Skin rash \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("Acetaminophen \n  aspirin \n ibuprofen \n ");
+		printf("INHOME REMEDIES ARE: \n ");
+		printf("Giloy \n Methi Leaves \n Papaya Leaves \n Turmeric \n ");
+	}
+	if(plants==6){
+		printf("SYMPTOMS:: \n ");
+		printf("Coughing that lasts three or more weeks \n Coughing up blood \n Chest pain \n pain with breathing or coughing \n Unintentional weight loss \n Fatigue \n Fever \n Night sweats \n Chills \n Loss of appetite \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n");
+		printf("kanamycin \n levofloxacin \n cycloserine \n ethionamide \n pyrazinamide \n ethambutol \n ");
+		printf("INHOME REMEDIES ARE: \n ");
+		printf("pineapples \n garlic \n Milk \n mint leaves \n ");
+	}
+	if(plants==7){
+		printf("SYMPTOMS:: \n ");
+		printf("Pain \n Stiffness \n Swelling \n Redness \n Decreased range of motion \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("Advil \n Motrin IB \n ");
+		printf("INHOME REMEDIES ARE: \n ");
+		printf(" Aquatic exercises \n Weight loss \n Yoga \n Hot and cold therapy \n ");
+	}
+	if(plants==8){
+		printf("SYMPTOMS:: \n ");
+		printf("one or more painful sores on part of the skin lining the mouth \n swollen skin around the sores \n problems with chewing or tooth brushing because of the tenderness \n irritation of the sores by salty, spicy or sour food \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("using a rinse of saltwater and baking soda \n placing milk of magnesia on the mouth ulcer \n covering mouth ulcers with baking soda paste \n using over-the-counter benzocaine (topical anesthetic) products like Orajel or Anbesol \n applying ice to canker sores \n ");
+		printf("INHOME REMEDIES ARE: \n");
+		printf(" Honey \n Baking Soda Paste \n Coconut Oil \n Orange Juice \n Coconut Milk \n ");
+	}
+	if(plants==9){
+		printf("SYMPTOMS:: \n ");
+		printf("Fever \n Sore throat \n Headache \n Vomiting \n Fatigue \n Back pain or stiffness \n Neck pain or stiffness \n Pain or stiffness in the arms or legs \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("Ibuprofen \n Diclofenac \n Acetaminofen \n ");
+		printf("INHOME REMEDIES ARE: \n ");
+		printf("Professional Massage \n Hydrotherapy \n Healthy Diet \n ");
+	}
+	if(plants==10){
+		printf("SYMPTOMS:: \n ");
+		printf("Pain \n Stiffness \n Swelling \n Redness \n Decreased range of motion \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("Advil \n Motrin IB \n ");
+		printf("INHOME REMEDIES ARE: \n ");
+		printf(" Aquatic exercises \n Weight loss \n Yoga \n Hot and cold therapy \n ");
+	}
+	if(plants==11){
+		printf("SYMPTOMS:: \n ");
+		printf("one or more painful sores on part of the skin lining the mouth \n swollen skin around the sores \n problems with chewing or tooth brushing because of the tenderness \n irritation of the sores by salty, spicy or sour food \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("using a rinse of saltwater and baking soda \n placing milk of magnesia on the mouth ulcer \n covering mouth ulcers with baking soda paste \n using over-the-counter benzocaine (topical anesthetic) products like Orajel or Anbesol \n applying ice to canker sores \n ");
+		printf("INHOME REMEDIES ARE: \n");
+		printf(" Honey \n Baking Soda Paste \n Coconut Oil \n Orange Juice \n Coconut Milk \n ");
+	}
+	if(plants==12){
+		printf("SYMPTOMS:: \n ");
+		printf("Fever \n Sore throat \n Headache \n Vomiting \n Fatigue \n Back pain or stiffness \n Neck pain or stiffness \n Pain or stiffness in the arms or legs \n ");
+		printf("***MEDICINE SUGGESTED IF AGE IS GREATER THAN 10 AND BELOW 50*** \n ");
+		printf("Ibuprofen \n Diclofenac \n Acetaminofen \n ");
+		printf("INHOME REMEDIES ARE: \n ");
+		printf("Professional Massage \n Hydrotherapy \n Healthy Diet \n ");
+	}
+	
+	
+	
+	
+	
+	
+	
 	return 0;
 }
--- src/plant.c
+++ src/plant.c
@@ -12,10 +12,42 @@ int plant(char a[100]){
 	{
 		return 3;
 	}
-		else if(strcmp(a,"Alternaria blight")==0)
+		else if(strcmp(a,"Greenaria bitter rot")==0)
 	{
 		return 4;	
-    	}
+    }
+	else if(strcmp(a,"Bacterial leaf spot")==0)
+	{
+		return 5;
+	}
+		else if(strcmp(a,"Alternaria blight")==0)
+	{
+		return 6;	
+    }
+	else if(strcmp(a,"Black rot")==0)
+	{
+		return 7;
+	}
+		else if(strcmp(a,"Blue mould rot")==0)
+	{
+		return 8;	
+    }
+	else if(strcmp(a,"Black mould rot")==0)
+	{
+		return 9;
+	}
+		else if(strcmp(a,"Green mould rot")==0)
+	{
+		return 10;	
+    }
+	else if(strcmp(a,"Rhizopus rot")==0)
+	{
+		return 11;
+	}
+		else if(strcmp(a,"Rust")==0)
+	{
+		return 12;	
+    }
     else{
     	printf("**please choose the disease name from the above list of diseases** \n");
 	}
